6a901fa803c9e5379ba10e325c6fa269999c0d14
AzureAD\alsaddigibrahim <alsaddig_ibrahim@stu.indianhills.edu>
2020-04-09 18:53:14 -0700

    added user list,models  user updated user create files

--- src/app/admin/users-config/list-users/list-users.component.ts
+++ src/app/admin/users-config/list-users/list-users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../Services/user.service';
+import { User } from "../../../Models/user.model";
+
 
 @Component({
   selector: 'app-list-users',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-users.component.css']
 })
 export class ListUsersComponent implements OnInit {
+users : User[] =[];
+// initialize our UserService methods on component construction
+  constructor(public userService: UserService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(){
+    // as soon as the page loads, get our users from the user service http call
+    this.userService.getUsers().subscribe(ResponseData => {
+      // users is now subscribed to, so that means we can place it in our html file.
+      // We can't access users outside this subscribe method.
+      this.users = ResponseData;
+    });
+  }
+  onDelete(userId:string){
+    this.userService.deleteUser(userId);
   }
 
 }
