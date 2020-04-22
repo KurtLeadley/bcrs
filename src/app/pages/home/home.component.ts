0d28db9573b19536cddf3ecca9ad8266143658ab
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-04-22 14:17:46 -0500

    Update home style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,15 +3,20 @@
  * Authors: Group 4
  * Description: bcrs
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('landing-page');
+  }
+
+  ngOnDestroy() {}
 }
