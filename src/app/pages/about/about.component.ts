a40d4cc83f5dff21aca034923d38cea007ae6e7d
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-04-25 23:56:52 -0500

    style updates

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('about-us');
   }
 
+  ngOnDestroy() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('about-us');
+  }
 }
