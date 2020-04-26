7396fce4982755f85821e047e147eb2b315f4b07
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-04-26 01:52:05 -0500

    graph continues

--- src/app/pages/admin/purchases-graph/purchases-graph.component.ts
+++ src/app/pages/admin/purchases-graph/purchases-graph.component.ts
@@ -1,68 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient,  HttpHeaders} from '@angular/common/http';
-import {Router} from '@angular/router';
-import { GraphService } from '../../../services/graph.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { InvoiceService } from '../../../services/invoice.service';
-import { throwMatDuplicatedDrawerError } from '@angular/material/sidenav';
-import { pipe } from "rxjs";
-import { map, take } from "rxjs/operators";
+import { ServiceService } from '../../../services/service.service';
+import { Invoice } from '../../../models/invoice.model';
+import { LineItem } from '../../../models/line-item.model';
+import { Service } from '../../../models/service.model';
 
 @Component({
   selector: 'app-purchases-graph',
   templateUrl: './purchases-graph.component.html',
-  styleUrls: ['./purchases-graph.component.scss']
+  styleUrls: ['./purchases-graph.component.scss'],
 })
 export class PurchasesGraphComponent implements OnInit {
-  productInfo: any
-  service: any
-  token: string
-  aggregates: any
-  aggregate: any
+  invoices: any;
+  data: any;
+  options: any;
+  labels;
+  services;
+  passwordReset = 0;
+  spywareRemoval = 0;
+  ramUpgrade = 0;
+  tuneup = 0;
+  keyboardCleaning = 0;
+  softwareInstallation = 0;
+  discCleanup = 0;
 
-  //for barchart
-  public barChartOptions:any = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-  public barChartType:string = 'bar';
-  public barChartLegend:boolean = true;
+  constructor(private invoiceService: InvoiceService, private sService: ServiceService) {}
 
-  //chart events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
-    public chartHovered(e:any):void {
-    console.log(e);
-  }
-
-   //chart columns and data
-   barChartLabels:string[] = ['Password Resets', 'Spyware Removals',
-  'Ram Upgrades', 'Software Installs', 'Tune Ups', 'Cleaned Keyboards',
-  'Cleaned Disks', 'Total $ x 10'];
-
-  //chart data copy before messing with it
-  public barChartData:any[] = [
-    {data: [0, 0, 0, 0, 0, 0, 0, 0], label: 'Units Sold'}
-  ];
-
-  constructor(public invoiceService: InvoiceService) {
-    this.invoiceService.getInvoicesForGraph().subscribe( data =>{
-     this.service=data
+  ngOnInit() {
+    this.sService.getServices().subscribe((res) => {
+      this.services = res;
     });
+    this.invoiceService.getInvoices().subscribe((res) => {
+      this.invoices = res;
 
-    this.invoiceService.getInvoicesForGraph().subscribe( res=>{
-      this.barChartData= [ res[0]['passwordCount'], res[0]['spyCount'],
-      res[0]['ramCount'], res[0]['softwareCount'], res[0]['tuneCount'],
-      res[0]['keyboardCount'], res[0]['diskCount'], res[0]['total']] ;
-    },
-    err =>{
+      const tempLabels = [];
+      const graphData = {};
 
+      for (const item of this.invoices) {
+        item.lineItems.forEach((e) => {
+          tempLabels.push(e.title);
+        });
+      }
+      console.log(tempLabels);
     });
-   }
-
-  ngOnInit(): void {
-
-
   }
-
 }
