306a2d2e29b3f6395df1dadd5da80bc6455c2b1d
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-04-29 10:35:47 -0500

    contact page

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgModule } from '@angular/core';
 
 @Component({
@@ -6,22 +6,16 @@ import { NgModule } from '@angular/core';
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss'],
 })
-export class ContactUsComponent implements OnInit {
-  firstName: string;
-  lastName: string;
-  email: string;
-  message: string;
-  youvisited: string;
-
+export class ContactUsComponent implements OnInit, OnDestroy {
   constructor() {}
 
-  ngOnInit() {}
-  submitForm() {
-    const message = `My first name Is ${this.firstName}.
-                    My last name ${this.lastName}.
-                    my email is ${this.email}.
-                    My message is ${this.message};
-                    Have you visited the Bob's Computer Repair Shop before? ${this.youvisited}.`;
-    alert(message);
+  ngOnInit() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('contact-page');
+  }
+
+  ngOnDestroy() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('contact-page');
   }
 }
